bdcb8f7e42a5a1f7e5b6d87afbdea90f548576f0
SyedImtiyaz-1 <syedimtiyazali141@gmail.com>
2025-07-09 15:33:19 +0530

    Greenx365

--- commit-gen.sh
+++ commit-gen.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Set your GitHub username and email
 git config user.name "SyedImtiyaz-1"
 git config user.email "syedimtiyazali141@gmail.com"
 
-# Loop over the number of days you want to backfill (e.g., 365 days)
 for i in {1..365}
 do
-  # Generate multiple commits per day
-  for j in {1..5}
+  for j in {1..20}
   do
     echo "$i-$j" >> commit-gen.txt
-    git add commits.txt
+    git add commit-gen.txt
     GIT_AUTHOR_DATE="$(date --date="$i days ago" +"%Y-%m-%dT12:00:00")" \
     GIT_COMMITTER_DATE="$(date --date="$i days ago" +"%Y-%m-%dT12:00:00")" \
     git commit -m "Commit $i-$j"
